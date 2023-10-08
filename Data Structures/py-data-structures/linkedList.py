c21e633fd31b639194faca3fd60802e66d5b7826
Jonash Gania <jonashtiglao@gmail.com>
2023-10-08 21:52:24 +0800

    append list method

--- Data Structures/py-data-structures/linkedList.py
+++ Data Structures/py-data-structures/linkedList.py
@@ -1,7 +1,21 @@
 # Linked lists
 
 class linked_list:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+    
+    # def append(value):
+        
+    def prepend(self,value):
+        self.head = Node(value, self.head)
+        self.size +=1
+
+    def size(self):
+         return self.size
 
 
 class Node:
-
+    def __init__(self,value, nextNode = None):
+            self.value = value
+            self.nextNode = nextNode
