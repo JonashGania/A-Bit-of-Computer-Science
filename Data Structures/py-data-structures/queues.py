40f85cda2a1ae1197490e8406edcc47b0534b829
Jonash Gania <jonashtiglao@gmail.com>
2023-10-09 22:23:48 +0800

    update

--- Data Structures/py-data-structures/queues.py
+++ Data Structures/py-data-structures/queues.py
@@ -2,8 +2,8 @@
 
 class Queue:
     def __init__(self):
-    
-    def enqueue(self):
-
-    def dequeue(self):
         
+    def enqueue(self):
+        # add new
+    def dequeue(self):
+        # remove
