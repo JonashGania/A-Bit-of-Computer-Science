1bf9c4f6d00e23f3f773fff39d0c167994b9063a
Jonash Gania <jonashtiglao@gmail.com>
2023-10-12 22:37:23 +0800

    added methods

--- Data Structures/js-data-structures/linkedList.js
+++ Data Structures/js-data-structures/linkedList.js
@@ -6,7 +6,7 @@ class linkedList{
         this.size = 0;
     }
 
-    append(value){
+    append(value){      // Adds a new node at the end of the list
         let node = new Node(value);
         let current;
 
@@ -25,12 +25,12 @@ class linkedList{
         this.size++;
     }
 
-    prepend(value){
+    prepend(value){     // adds new node to the start of the list
         this.head = new Node(value, this.head);
         this.size++;
     }
 
-    insertAt(value, index){
+    insertAt(value, index){     // insert new node at the given index
         if(index > 0 && index > this.size){
             return;
         }
@@ -57,30 +57,131 @@ class linkedList{
         this.size++;
     }
 
-    size(){
+    removeAt(index){    // removes a node in a given index
+        if(index > 0 && index > this.size){
+            return;
+        }
+
+        let current = this.head;
+        let previous;
+        let count = 0;
+
+        if(index === 0){
+            this.head = current.next;
+        } else {
+            while(count < index){
+                count++;
+                previous = current;
+                current = current.next;
+            }
+
+            previous.next = current.next;
+        }
+        this.size--;
+    }
+
+    size(){     // return list size
         return this.size;
     }
 
-    // head(){
+    head(){     // return first node
+        return this.head.value;
+    }
+
+    tail(){     // return last node
+        let current = this.head;
+
+        while(current && current.next){
+            current = current.next;
+        }
+
+        console.log(current.value);
+    }
+
+    getAt(index){       // return node at given index
+        let current = this.head;
+        let count = 0
         
-    // }
+        while(current){
+            if (count === index){
+                console.log(current.value);
+                
+            }
+            count++;
+            current = current.next;
+        }
 
-    // tail
-        // get lists tail node
-
-    // get at(index)
-        // get list at index
+        return null
+    }
         
-    // pop
+    pop(){      // remove last element of the list
+        if(!this.head){
+            return
+        }
 
-    // print(){
-    //     let current = this.head;
+        if(!this.head.next){
+            this.head = null;
+            this.size = 0;
+            return;
+        }
 
-    //     while(current){
-    //         console.log(current.value);
-    //         current = current.next;
-    //     }
-    // }
+        let current = this.head;
+        let previous
+
+        while(current.next){
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = null;
+        this.size--;
+    }
+    
+    contains(value){    // return true if value is in the list, otherwise false
+        let current = this.head;
+
+        while(current){
+            if(current.value === value){
+                return true;
+            }
+
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    find(value){    // find the index of the given node value
+        let current = this.head;
+        let index = 0;
+
+        while(current){
+            if(current.value === value){
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    toString(){     // print the node values
+        if(!this.head){
+            return;
+        }
+        
+        let current = this.head;
+        let stringList = "";
+
+        while(current){
+            stringList += `(${current.value}) -> `
+            current = current.next;
+        }
+
+        stringList += "null";
+        console.log(stringList);
+    }
 }
 
 class Node{
@@ -95,7 +196,9 @@ const list = new linkedList();
 list.prepend(100);
 list.prepend(300);
 list.append(400);
-list.insertAt(500, 2);
+list.append(800);
+list.insertAt(500, 0);
+list.pop();
 
-console.log(list);
+list.toString()
 
