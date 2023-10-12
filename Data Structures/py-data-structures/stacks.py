af6c9b00ad3a436057cceba9fbc25583850df9dc
Jonash Gania <jonashtiglao@gmail.com>
2023-10-12 22:39:58 +0800

    added functions

--- Data Structures/py-data-structures/stacks.py
+++ Data Structures/py-data-structures/stacks.py
@@ -6,8 +6,14 @@ class Stack:
 
     def add(self, element):
         return self.items.append(element)
+    
     def remove(self):
         length = len(self.items)
 
         if length > 0:
             return self.items.pop()
+        
+    def peek():
+
+    def size():
+        
