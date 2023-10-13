24d9a04c0ee64d82b7c54c3e051727f1df4b902b
Jonash Gania <jonashtiglao@gmail.com>
2023-10-13 22:49:25 +0800

    add and remove methods

--- Algorithms/py-algo/BinarySearchTree.py
+++ Algorithms/py-algo/BinarySearchTree.py
@@ -2,9 +2,31 @@
 
 
 class Node:
-    def __init__(self):
-        
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
 class Tree:
     def __init__(self):
-        
+        self.root = None
+
+    def insert(self, data):
+        newNode = Node(data)
+
+        if self.root == None:
+            self.root = newNode
+        else:
+            self.insert_node(self.root, newNode)
+
+    def insert_node(self, node, new_node):
+        if new_node.data < node.data:
+            if node.left == None:
+                node.left = new_node
+            else:
+                self.insert_node(node.left, new_node)
+        else:
+            if node.right == None:
+                node.right = new_node
+            else:
+                self.insert_node(node.rigt, new_node)
