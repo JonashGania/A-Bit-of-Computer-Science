97458579729312e6a788406d007783b5a722782c
Jonash Gania <jonashtiglao@gmail.com>
2023-10-13 15:54:32 +0800

    print stacks

--- Data Structures/py-data-structures/stacks.py
+++ Data Structures/py-data-structures/stacks.py
@@ -4,16 +4,38 @@ class Stack:
     def __init__(self):
         self.items = []
 
-    def add(self, element):
+    def add(self, element):     # add an element at the top of the stack
         return self.items.append(element)
     
-    def remove(self):
+    def remove(self):   # remove the topmost element of the stack
         length = len(self.items)
 
-        if length > 0:
-            return self.items.pop()
+        if length == 0:
+            return "Underflow"
         
-    def peek():
+        return self.items.pop()
+        
+    def peek(self):     # peek the topmost element of the stack
+        last = len(self.items) - 1
+        return self.items[last]
 
-    def size():
+    def size(self):     # return the size of the stack
+        return len(self.items)
+    
+    def print(self):       # print stack
+        string = ""
+
+        for item in self.items:
+            string += str(item) + " "
         
+        return string
+
+stack = Stack()
+
+stack.add(10)
+stack.add(20)
+stack.add(30)
+stack.add(40)
+stack.remove()
+
+print(stack.print())
