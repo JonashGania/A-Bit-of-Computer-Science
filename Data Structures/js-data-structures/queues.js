3dd436396cdd3a5d974cbc677f620f44768eb03e
Jonash Gania <jonashtiglao@gmail.com>
2023-10-13 15:53:40 +0800

    print queues

--- Data Structures/js-data-structures/queues.js
+++ Data Structures/js-data-structures/queues.js
@@ -7,19 +7,42 @@ class Queue{
         this.backIndex = 0;
     }
 
-    enqueue(item){
-        // add queue
+    enqueue(item){      // add elements at the end of queue
+        this.items[this.backIndex] = item
+        this.backIndex++
+        return item 
     }
 
-    dequeue(){
-        // remove 
+    dequeue(){      // remove elements at the front of queue
+        const item = this.items[this.frontIndex]
+        delete this.items[this.frontIndex]
+        this.frontIndex++
+        return item
     }
 
-    peek(){
-
+    peek(){     // peek the front element of the queue
+        return `front -> ${this.items[this.frontIndex]}`
     }
 
-    size(){
-        
+    size(){     // return the size of queue
+        return this.backIndex - this.frontIndex
     }
+
+    printQueue(){       // print queue
+        return this.items
+    }
+
 }
+
+const queue = new Queue();
+
+console.log(queue.enqueue(10));
+console.log(queue.enqueue(20));
+console.log(queue.enqueue(30));
+queue.dequeue()
+
+console.log(queue.peek())
+console.log(queue.size())
+
+const print = queue.printQueue()
+console.log(print)
