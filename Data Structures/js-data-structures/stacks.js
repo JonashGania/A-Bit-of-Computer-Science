31ea0bf5b68d5c8109ddb93e0b59ca434ad9f2dd
Jonash Gania <jonashtiglao@gmail.com>
2023-10-13 15:53:55 +0800

    print stacks

--- Data Structures/js-data-structures/stacks.js
+++ Data Structures/js-data-structures/stacks.js
@@ -5,21 +5,45 @@ class Stack{
         this.items = [];
     }
 
-    add(element){
+    add(element){   // add an element at the top of the stack
         return this.items.push(element);
     }
 
-    remove(){
-        if(this.items.length > 0){
-            return this.items.pop()
+    remove(){   // removes the the topmost element of the stack
+        if(this.items.length === 0){
+            return 'Undeflow'
         }
+
+        return this.items.pop()
     }
 
-    peek(){
-        
+    peek(){     // peek the topmost element of the stack
+        return this.items[this.items.length - 1]
     }
 
-    size(){
-        
+    size(){     // return the size of the stack
+        return this.items.length
+    }
+
+    print(){      // print stack
+        let str = ""
+
+        for(let i = 0; i < this.items.length; i++){
+            str += this.items[i] + " ";
+        }
+
+        return str
     }
 }
+
+const stack = new Stack()
+
+stack.add(100);
+stack.add(200);
+stack.add(300);
+stack.add(400);
+stack.remove();
+
+console.log(stack.peek())
+console.log(stack.size())
+console.log(stack.print())
