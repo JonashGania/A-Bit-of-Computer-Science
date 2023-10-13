ae3388578ac88385d92e2f80056e6771fd9408ad
Jonash Gania <jonashtiglao@gmail.com>
2023-10-13 22:49:03 +0800

    add and remove methods

--- Algorithms/js-algo/BinarySearchTree.js
+++ Algorithms/js-algo/BinarySearchTree.js
@@ -2,13 +2,49 @@
 
 
 class Node{
-    constructor(){
-
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 }
 
 class Tree{
     constructor(){
+        this.root = null
+    }
 
+    insert(data){
+        const newNode = new Node(data);
+
+        if(this.root === null){
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode){
+        if(newNode.data < node.data){
+            if(node.left === null){
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if(node.right === null){
+                node.right = newNode
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    remove(data){
+        this.root = this.removeNode(this.root, data);
+    }
+
+    removeNode(node, key){
+        
     }
 }
