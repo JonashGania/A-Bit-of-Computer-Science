ff44d42a36f984e2261f3e806fef3124b00ef6c2
Jonash Gania <jonashtiglao@gmail.com>
2023-10-14 14:54:57 +0800

    add and remove nodes

--- Algorithms/py-algo/BinarySearchTree.py
+++ Algorithms/py-algo/BinarySearchTree.py
@@ -29,4 +29,60 @@ class Tree:
             if node.right == None:
                 node.right = new_node
             else:
-                self.insert_node(node.rigt, new_node)
+                self.insert_node(node.right, new_node)
+
+    def remove(self, data):
+        self.root = self.remove_node(self.root, data)
+
+    def remove_node(self, node, key):
+        if node == None:
+            return None
+        
+        elif key < node.data:
+            node.left = self.remove_node(node.left, key)
+            return node
+        
+        elif key > node.data:
+            node.right = self.remove_node(node.right, key)
+            return node
+        
+        else:
+            if node.left == None and node.right == None:
+                node = None
+                return node
+            
+            if node.left == None:
+                node = node.left
+                return node
+            
+            elif node.right == None:
+                node = node.right
+                return node
+            
+            aux = self.find_min_node(node.right)
+            node.data = aux.data
+
+            node.right = self.remove_node(node.right, aux.data)
+            return node
+
+    def find_min_node(self,node):
+        if node.left == None:
+            return None
+
+        else:
+            return self.find_min_node(node.left)
+        
+    def get_root_node(self):
+        return self.root
+    
+
+bst = Tree()
+
+bst.insert(23)
+bst.insert(22)
+bst.insert(43)
+bst.insert(56)
+bst.insert(6)
+bst.insert(3)
+
+print(bst.get_root_node())
